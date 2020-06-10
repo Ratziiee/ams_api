d20ec61f1d751ea7c058bb29d99478e5df0e3cd8
Rajat Upadhyay <31692636+Ratziiee@users.noreply.github.com>
2020-06-11 01:45:12 +0530

    3 new api

--- API/controllers/controller.js
+++ API/controllers/controller.js
@@ -33,4 +33,60 @@ module.exports.getAllEmployees = (req,res) => {
     });
 }
 
+module.exports.postAddEmployee = (req,res) => {
+
+    let name=req.query.name;
+    let email=req.query.email;
+    let mobile=req.query.mobile;
+    let designation = req.query.designation;
+    let department = req.query.department;
+    let emp_id = req.query.emp_id;
+    let employee_type = req.query.employee_type;
+
+    var query = `INSERT INTO public.user_master(
+        name, email, mobile, designation, department, emp_id, emp_type)
+        VALUES ('${name}', '${email}', ${mobile}, '${designation}', '${department}', '${emp_id}', '${employee_type}')`;
+    debugger
+    db.any(query).then((data) => {
+        console.log('data aaya',data);
+        // utils.sendMail(req,res,"AeroGMS","ratzupadhyay@gmail.com","Welcome to AeroGMS",response_msgs.signup_mail,"");
+        res.send(data);
+    }).catch((err) => {
+        console.log('error aaya',err);
+        res.send(err);
+    });
+}
+
+module.exports.getAllEmployeesLogs = (req,res) => {
+    var query = `SELECT id, emp_id, mobile, "timestamp", status
+	FROM public.attendance_master`;
+    debugger
+    db.any(query).then((data) => {
+        console.log('data aaya',data);
+        // utils.sendMail(req,res,"AeroGMS","ratzupadhyay@gmail.com","Welcome to AeroGMS",response_msgs.signup_mail,"");
+        res.send(data);
+    }).catch((err) => {
+        console.log('error aaya',err);
+        res.send(err);
+    });
+}
+
+module.exports.getSingleEmployeesLogs = (req,res) => {
+
+    let emp_id = req.query.emp_id;
+    let mobile = req.query.mobile;
+
+    var query = `SELECT id, emp_id, mobile, "timestamp", status
+	FROM public.attendance_master where emp_id = '${emp_id}' and mobile = ${mobile}`;
+    debugger
+    db.any(query).then((data) => {
+        console.log('data aaya',data);
+        // utils.sendMail(req,res,"AeroGMS","ratzupadhyay@gmail.com","Welcome to AeroGMS",response_msgs.signup_mail,"");
+        res.send(data);
+    }).catch((err) => {
+        console.log('error aaya',err);
+        res.send(err);
+    });
+}
+
 
