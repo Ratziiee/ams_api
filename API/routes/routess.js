974538976eda6ce3a4e8f79a9b345e252432b12a
Rajat Upadhyay <31692636+Ratziiee@users.noreply.github.com>
2020-06-11 01:44:31 +0530

    add 3 new API,
     getAllEmployee Logs , getSingleEmployee Logs ,  add employee

--- API/routes/routess.js
+++ API/routes/routess.js
@@ -5,6 +5,10 @@ const controller = require('../controllers/controller');
 router.get('/', controller.test);
 router.get('/getAllEmployees',controller.getAllEmployees)
 
+router.get('/getAllEmployeesLogs',controller.getAllEmployeesLogs)
+router.get('/getSingleEmployeesLogs',controller.getSingleEmployeesLogs)
+
+router.post('/postAddEmployee',controller.postAddEmployee)
 
 
 module.exports = router;
