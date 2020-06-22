54095748225e5f7154a58c8ec6c45551bc8c97f3
rajat upadhyay <ratzupadhyay@gmail.com>
2020-06-22 12:03:55 +0530

    rresponse Edit in add Employee

--- API/controllers/controller.js
+++ API/controllers/controller.js
@@ -50,7 +50,8 @@ module.exports.postAddEmployee = (req,res) => {
     db.any(query).then((data) => {
         console.log('data aaya',data);
         // utils.sendMail(req,res,"AeroGMS","ratzupadhyay@gmail.com","Welcome to AeroGMS",response_msgs.signup_mail,"");
-        res.send(data);
+        res.send({statusCode : 200, message : "Data Successfully Saved", data:data});
+        // res.send(data);
     }).catch((err) => {
         console.log('error aaya',err);
         res.send(err);
